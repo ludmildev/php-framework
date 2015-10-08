0923eb21d95e8f2623be0ae97528f6bc9df81531
ludmil Zdravkov <ludmildev@gmail.com>
2015-10-08 19:42:14 +0300

    Update database.php

--- shopping_card/config/database.php
+++ shopping_card/config/database.php
@@ -1,7 +1,7 @@
 <?php
 $conf['default']['url'] = 'mysql:host=localhost;dbname=test';
 $conf['default']['username'] = 'root';
-$conf['default']['pass'] = 'delux919';
+$conf['default']['pass'] = '';
 $conf['default']['pdo_options'][PDO::MYSQL_ATTR_INIT_COMMAND] = "SET NAMES 'UTF8'";
 $conf['default']['pdo_options'][PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
 
